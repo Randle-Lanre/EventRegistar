fba2c740f1a025ab4f35d2cff30d32a04d6a1af7
Randle-lanre <kennyrandle09@hotmail.co.uk>
2021-02-15 22:28:58 +0000

    added Post service for event registar

--- participant-register/src/app/Services/registar-service.service.ts
+++ participant-register/src/app/Services/registar-service.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { Registar } from './registar';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistarServiceService {
+apiUrl = 'api/events'
+
 
   constructor(private http: HttpClient) { }
+
+
+
+  addUserToRegistar( registar: Registar): Observable<Registar>{
+    return this.http.post<Registar>(this.apiUrl, registar, httpOptions);
+  }
 }
