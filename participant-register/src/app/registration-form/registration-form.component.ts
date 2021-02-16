5f66f609bfbc763d2610f7f1a0a8957c9f6dfc9c
Randle-lanre <kennyrandle09@hotmail.co.uk>
2021-02-16 19:24:30 +0000

    added save skeleton in components form

--- participant-register/src/app/registration-form/registration-form.component.ts
+++ participant-register/src/app/registration-form/registration-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Registar } from '../Services/registar';
+import { RegistarServiceService } from '../Services/registar-service.service';
 
 
 @Component({
@@ -9,16 +11,33 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegistrationFormComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private registarServiceService: RegistarServiceService) { }
 
    userRegistrationForm = this.fb.group({
-     firstName: [''],
-     lastname: [''],
-     userComment: ['']
+     firstName: ['', Validators.required],
+     lastName: ['', Validators.required],
+     userComment: ['', Validators.required]
    })
+
+    addUserToRegistar(userRegistrationInfo){
+
+      console.log(this.userRegistrationForm.value)
+     // this.addUserToRegistar(userRegistrationInfo)
+      window.alert('form submitted');
+
+
+    }
+ 
+
+ 
+
+  //  add(registryEntry: string): void{
+  //    this.
+  //  }
 
   ngOnInit(): void {
     
   }
+
 
 }
