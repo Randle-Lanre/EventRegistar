c0f60b7a46fec280a22a378ed34cd53ac6f7e074
Randle-lanre <kennyrandle09@hotmail.co.uk>
2021-02-18 22:04:59 +0000

    created db migrations and updated db with initial model

--- RegistarApi/Model/EventRegistar.cs
+++ RegistarApi/Model/EventRegistar.cs
@@ -1,13 +1,22 @@
-﻿namespace RegistarApi.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistarApi.Model
 {
     public class EventRegistar
     {
 
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(12)]
         public string FirstName { get; set; }
 
+        [Required]
+        [StringLength(12)]
         public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string ParticipantComment { get; set; }
         
     }
