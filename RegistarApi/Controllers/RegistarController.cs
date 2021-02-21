9acad219f91c963f53ef32416b15f613e54c79dc
Randle-lanre <kennyrandle09@hotmail.co.uk>
2021-02-21 21:53:48 +0000

    added delete and update methods to api controller

--- RegistarApi/Controllers/RegistarController.cs
+++ RegistarApi/Controllers/RegistarController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RegistarApi.Model;
 
 namespace RegistarApi.Controllers
@@ -16,10 +17,14 @@ namespace RegistarApi.Controllers
 
         }
 
-        [HttpGet]
-        public ActionResult GetRegistar()
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EventRegistar>> GetRegistar(int id)
         {
-            return Ok();
+            var anEvent = await _context.EventRegistars.FindAsync(id);
+
+            if (anEvent != null)
+                return Ok(anEvent);
+            return NotFound();
         }
 
         [HttpPost]
@@ -30,6 +35,41 @@ namespace RegistarApi.Controllers
            await _context.SaveChangesAsync();
 
            return CreatedAtAction(nameof(GetRegistar), new {id = anEvent}, anEvent);
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteRegistarItem(int id)
+        {
+            var registarItem = await _context.EventRegistars.FindAsync(id);
+
+            if (registarItem == null)
+                return StatusCode(404);
+            _context.EventRegistars.Remove(registarItem);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEventRegistar (int id, EventRegistar eventRegistar)
+        {
+            if (id != eventRegistar.Id)
+                return BadRequest();
+
+            _context.Entry(eventRegistar).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+
+            }
+
+            return NoContent();
         }
         
     }
