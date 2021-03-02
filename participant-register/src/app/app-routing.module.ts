a5c07db9fa460eca88e4743f7442307eb88c2a2b
Randle-lanre <kennyrandle09@hotmail.co.uk>
2021-03-02 22:32:10 +0000

    added registerlist componenet to router and setup dummy table to show list of all participants

--- participant-register/src/app/app-routing.module.ts
+++ participant-register/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterlistComponent } from './registerlist/registerlist.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 
 const routes: Routes = [
-  {path: '', component: RegistrationFormComponent}
+  {path: '', component: RegistrationFormComponent},
+  {path: '/registerlist', component:RegisterlistComponent}
 ]
 
 
