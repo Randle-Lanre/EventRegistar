a80d2b30f015850d7a6a02e0c37610c8e97e26bc
Randle-lanre <kennyrandle09@hotmail.co.uk>
2021-03-04 22:48:42 +0000

    working on base test db setup

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class BaseTest
+    {
+
+        protected ApplicationDbContext BuildContext(string databaseName)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName).Options;
+
+            var dbContext = new ApplicationDbContext(options);
+            return dbContext;
+        }
+
+
+    }
+}
